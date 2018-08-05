1adf3ec5d1226924809aacfffa1415ddda5ef84b
marwa <marwa@marwas-mac.local>
2018-08-05 06:42:59 -0700

    MVC and Adding new item functionality

--- Todoey/Controller/TodoeyTableViewController.swift
+++ Todoey/Controller/TodoeyTableViewController.swift
@@ -14,12 +14,14 @@ class TodoeyTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arr.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
         cell.textLabel?.text = arr[indexPath.row]
         
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -28,12 +30,31 @@ class TodoeyTableViewController: UITableViewController {
             tableView.cellForRow(at: indexPath )?.accessoryType = .checkmark
         }
     }
-    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        
+    
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+        var txtFild = UITextField()
+        let addAlert = UIAlertController(title: "ToDo", message: "", preferredStyle: .alert)
+        let addAction = UIAlertAction(title: "Add New Item", style: .default) { (action) in
+            
+            if (txtFild.text?.isEmpty)!{
+                let wrongAlert = UIAlertController(title: "ToDo", message: "Please type something", preferredStyle: .alert)
+                let wrongAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                wrongAlert.addAction(wrongAction)
+                self.present(wrongAlert, animated: true, completion: nil)
+            
+            }else{
+                self.arr.append(txtFild.text!)
+                self.tableView.reloadData()
+            }
+        }
+            
+        addAlert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add New Item!"
+            txtFild = alertTextField
+        }
+        addAlert.addAction(addAction)
+        present(addAlert, animated: true, completion: nil)
     }
-    
-    
-    
     
     
     
